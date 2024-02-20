41a9f19cee40f5f7d97e329cc99a4345c21bb69c
Anjali Kumari <113256438+anjali074@users.noreply.github.com>
2024-02-20 11:12:07 +0530

    Add files via upload

---
+++ Amazon.js
@@ -0,0 +1,8 @@
+const product={
+fullname:"Ballpen",
+rating:4,
+price:270,
+offer:5,
+
+}
+console.log(product.fullname);
---
+++ instagram.js
@@ -0,0 +1,10 @@
+//creat a constant object calld"price to store information show in tepicture
+const profile={
+  Fullname:"priya",
+  follower:234,
+  folowing:243,
+  isfllow:true,
+  post:123,
+  information:maydetail,
+  }
+  console.log(profile);
