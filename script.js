d9efda7869b3b2ebedbdb911e7f746722115f7aa
Anjali Kumari <113256438+anjali074@users.noreply.github.com>
2024-02-21 11:12:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,89 @@
+//Arthmetic operator
+/*let a=5;
+let b=10;*/
+/*console.log("a=",a ,"&b=",b);//value
+console.log("a+b=",a+b);//Addition
+console.log("a-b=",a-b);//substraction
+console.log("a*b=",a*b);//Multiplication
+console.log("a/b=",a/b);//Divide
+console.log(" a%b=",a%b);//modulus
+console.log("a**b=",a**b);//exponentation means 5^10*/
+
+//unary operator
+/*let a=5;
+let b=10;
+
+console.log("a=",a ,"&b=",b);
+a++;//a+1
+b++;//b+1
+console.log("a=", a)
+console.log("b=", b)
+a--; //a-1
+console.log("a=", a);
+b--; //b-1
+console.log("b=", b);
+console.log("a++=",a++);//post increment
+console.log("a=", a);
+console.log("a--=", a--);//post decrement
+console.log("a=", a); 
+console.log("++a=", ++a);//pre increment
+console.log("--a",--a);//pre decrement*/
+//Assigment operator
+/*let x=10;
+let y=12;
+//console.log("x=", x, "y=",y);
+//x+=4;//x=x+4
+//console.log("x=", x);
+//y+=5;//y=y+5
+//console.log("y=", y);
+//x-=2;//x=x-2
+//console.log("x=", x);
+//y-=4;//y=y-4
+//console.log("y=", y);
+//x/=3;
+//console.log("x=", x);
+y/=4;//y=y/4
+console.log("y=", y);
+x*=2;//x=x*2 
+console.log("x=", x);
+y*=3;
+console.log("y=", y);
+x%=7;
+console.log("x=", x);
+y%=6;
+console.log("y=", y);
+x**=2;
+console.log("x=", x);
+y**=5;
+console.log("y=", y);*/
+
+//comparision operator
+/*let x=10; 
+let y=12;
+console.log("x=", x,"y=", y);
+console.log("x==y", x==y);
+console.log("x===y", x===y);
+console.log("x!=y", x!=y);
+console.log("x!==y",x!==y);
+console.log("x>y",x>y);
+console.log("x>=y",x>=y);
+console.log("x<y", x<y);
+console.log("x<=y", x<=y);*/
+
+//logical operator
+
+let x=10; 
+let y=12;
+let cond1=x>y//true
+let cond2=x==10//true
+console.log("x=", x,"y=", y);
+console.log("cond1 && cond2=",cond1 && cond2);
+//then
+let cond3=x<y//true
+let cond4=x==y//false
+console.log("cond3 && cond 4=", cond3 && cond4);
+//or
+console.log("cond1 ||cond2=", x<y||x===y);//pipe symbol
+
+//NOT
+console.log("cond1 ! cond2=", !(x>y ));
