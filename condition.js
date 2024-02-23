7378df0a3f98ffc5fa47ffb50084ecd69785d654
Anjali Kumari <113256438+anjali074@users.noreply.github.com>
2024-02-23 11:06:53 +0530

    Add files via upload

---
+++ condition.js
@@ -0,0 +1,87 @@
+//condition operator
+//if condition?
+/*let age=25;
+if(age>18){
+  console.log("you can vote")
+}*/
+/*let  mode="dark";//light
+let color;
+if(mode==="dark"){
+  color="black";
+}
+if(mode==="light"){
+  color="white";
+}
+console.log(color);*/
+/*let  mode="dark";//light
+let color;
+if(mode==="dark"){ // if -else condition
+  color="black";
+}
+else{
+  color="white";
+}
+console.log(color);*/
+// other example Age
+/*let age=25;
+if(age>=18){
+  console.log(yes);
+}
+else{
+  console.log(No);
+}*/
+
+//Q- find the even and odd
+/*let num=5;
+if(num%2===0){
+  console.log( num,"even");
+}
+else {
+  console.log(num,"odd")
+}*/
+//else-if statement
+
+/*let age=25;
+if(age<18){
+  console.log("junior");
+}
+else if(age>20){
+  console.log("senior");
+}
+else{
+  console.log("middle");
+}*/
+ //other case
+ /*let mode="black";// All color print change this mode
+ let color;
+ if(mode==="dark"){
+  color="dark";
+  //console.log("dark");
+ }
+ else if(mode==="blue"){
+  //console.log("blue");
+  color="blue";
+ }
+ else if(mode==="pink"){
+  //console.log("pink");
+  color="pink";
+ }
+ else if(mode==="red"){
+  //console.log("red");
+  color="red";
+ }
+ else{
+ // console.log("white");
+ color="white";
+ }
+ console.log(color);
+ //without block creat //not error this line then only one work not write   way // generally use with block use right way
+ if(mode==="black") console.log(mode);*/
+
+ //Ternary operator
+ let age=25;
+ //if else write in other way short term in generally use , long term use if -else condition right way
+  let result =age>18? "adult": "not adult";// simple, compact 
+  console .log(result);
+
+  
