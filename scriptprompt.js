56c36dc2324ee2bd2f8173ac64a47de0b7571b57
Anjali Kumari <113256438+anjali074@users.noreply.github.com>
2024-02-23 11:57:19 +0530

    Add files via upload

---
+++ scriptprompt.js
@@ -0,0 +1,12 @@
+/*alert("hello");//one time popup
+prompt("hello"); 
+let name=prompt("hello"); 
+console.log(name); */
+// Q-get user to input a number using promot enter anumber check if th number is a multile of 5 root
+let  num =prompt("Enter a number");
+if(num%5===0){
+  console.log("number is multile value of 5");
+}
+else{
+  console.log("not amultiple value of 5");
+}
